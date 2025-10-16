bd29d560cdb858afbe54188e03cc0c823526be58
Gabo <gra.gaod312@gmail.com>
2025-10-15 21:36:12 -0600

    correcion 2

--- practica2_4A/practica4A/practica4A.ino
+++ practica2_4A/practica4A/practica4A.ino
@@ -1,26 +1,40 @@
-#define btn 2
-#define bu 4
-bool bandera = false;
+
+#include <WiFi.h>
+
+const char* ssid = "Mega-2.4G-57AE";
+const char* password = "RT2tXTARhB";
+
+WiFiServer serve(8888);
+WiFiClient client;
 
 void setup() {
-  pinMode(btn,INPUT);
-  pinMode(bu,OUTPUT);
+  // put your setup code here, to run once:
   Serial.begin(115200);
+  WiFi.begin(ssid,password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.println("Conectandose....");
+  }
+  Serial.println("Cliente conectado");
+  Serial.println(WiFi.localIP());
+  //Es importante ingresar la linea de comando serve.begin() esto para iniciar el servidor y realizar peticiones
+  serve.begin();
 }
 
 void loop() {
-  int estado = digitalRead(btn);
-  delay(500);
-  if (estado == 1){
-    bandera = !bandera;
-    if (bandera== true){
-      tone(bu,2000);
-      Serial.println("sonido");
-    }
-    
-  }else{
-      noTone(bu);
-    
-  }
+  // int r = random(0,255);
+  int r = random(0,100);
+  int g = random(0,255);
+  int b = random(0,255);
+  String colores = String(r) + "-" + String(g) + "-" + String(b);
   
+  //Nota cuando el esp32 envia la informacion este se desconecta de WiFiClient
+  //Por tal razon es necesario volver a inicializarlo con WiFiServer.available();
+  if(!client || !client.connected()){
+    //Vuelve a conectar
+    client = serve.available();
+  }
+  client.println(r);
+  Serial.println(r);
+  delay(500);
 }
